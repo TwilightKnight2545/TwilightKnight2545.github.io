005b6df9af1090e841c795e00dc961b977252f78
Matthew Wickstrom <18mjwickstrom@gmail.com>
2025-03-05 14:34:32 -0600

    new file:   MJ_AW_FA_17770557/images/recipes/10sloppy-joes.png 	new file:   MJ_AW_FA_17770557/images/recipes/11chicken-fajitas.png 	new file:   MJ_AW_FA_17770557/images/recipes/12ground-beef-tacos.png 	new file:   MJ_AW_FA_17770557/images/recipes/13black-bean-soup.png 	new file:   MJ_AW_FA_17770557/images/recipes/14chicken-stir-fry.png 	new file:   MJ_AW_FA_17770557/images/recipes/15stuffed-bell-peppers.png 	new file:   MJ_AW_FA_17770557/images/recipes/5-cheesy-broccoli-soup.png 	new file:   MJ_AW_FA_17770557/images/recipes/6french-toast.png 	new file:   MJ_AW_FA_17770557/images/recipes/7fried-egg-sandwich.png 	new file:   MJ_AW_FA_17770557/images/recipes/8hashbrowns.png 	new file:   MJ_AW_FA_17770557/images/recipes/9chicken-and-rice-casserole.png 	modified:   MJ_AW_FA_17770557/js/recipes.js 	modified:   MJ_AW_FA_17770557/pages/food/index.html 	modified:   MJ_AW_FA_17770557/styles/header-footer.css 	modified:   MJ_AW_FA_17770557/styles/main.css 	modified:   MJ_AW_FA_17770557/styles/recipes.css

--- MJ_AW_FA_17770557/js/recipes.js
+++ MJ_AW_FA_17770557/js/recipes.js
@@ -32,8 +32,8 @@ const createTag = (innerText, isTagInfo) =>{ //if isTagInfo is true
 
     return tag;
 }
-const addRecipe = (name, description, imagePath, link, prepTime, calories, tags) =>{
-    recipieList.push([name, description, imagePath, link, prepTime, calories, tags]);
+const addRecipe = (name, description, imagePath, link, prepTime, servings,calories, tags) =>{
+    recipieList.push([name, description, imagePath, link, prepTime, servings, calories, tags]);
 }
 
 //--Functions
@@ -42,7 +42,7 @@ const setupInitialOptions = () =>{
     selectorOptions = []; //Clears array, [displayText, value]
     addOption("Filter…", "default");
     // addOption("Meal Type", "seperator");
-    // addOption("Breakfast", "breakfast");
+    addOption("Breakfast", "breakfast");
     // addOption("Lunch", "lunch");
     // addOption("Dinner", "dinner");
     addOption("Dietary Restrictions", "seperator");
@@ -58,14 +58,43 @@ const setupInitialOptions = () =>{
     filterList = selectorOptions;
 }
 const loadRecipes = () =>{
-    // Add recipes here
-    addRecipe("Easy Spaghetti", "'This Easy Spaghetti Recipe is ready in just 15 minutes and is a hearty, comforting meal during cooler months. Pair with a side salad or steamed veggies for a full meal and enjoy!' - Averie Sunshine", "../../images/recipes/1spaghetti.png", "https://www.averiecooks.com/easy-15-minute-spaghetti/#:~:text=from%2021%20votes-,Easy%20Spaghetti,-By%20Averie%20Sunshine", "15m", "941", ["under-fifteen-prep"]);
+    // Add recipes here (Broken up into groups of five to make it easier to search)
+    // 1-5
+    addRecipe("Easy Spaghetti", "'This Easy Spaghetti Recipe is ready in just 15 minutes and is a hearty, comforting meal during cooler months. Pair with a side salad or steamed veggies for a full meal and enjoy!' - Averie Sunshine", "../../images/recipes/1spaghetti.png", "https://www.averiecooks.com/easy-15-minute-spaghetti/#:~:text=from%2021%20votes-,Easy%20Spaghetti,-By%20Averie%20Sunshine", "15m", "4-6","941", ["under-fifteen-prep"]);
 
-    addRecipe("Easy Homemade Mac and Cheese", "'This Easy Homemade Mac and Cheese Recipe is made with 6 ingredients in 15 minutes on the stovetop (no baking required)! Time to ditch the boxed mac and cheese for this irresistibly creamy, smooth & cheesy homemade recipe!' - Laura", "../../images/recipes/2mac-and-cheese.png", "https://joyfoodsunshine.com/homemade-mac-and-cheese/#:~:text=PRINT-,Easy%20Homemade%20Mac%20and%20Cheese%20Recipe,-LAURA", "15m", "271.1", ["vegetarian", "under-fifteen-prep"]);
+    addRecipe("Easy Homemade Mac and Cheese", "'This Easy Homemade Mac and Cheese Recipe is made with 6 ingredients in 15 minutes on the stovetop (no baking required)! Time to ditch the boxed mac and cheese for this irresistibly creamy, smooth & cheesy homemade recipe!' - Laura", "../../images/recipes/2mac-and-cheese.png", "https://joyfoodsunshine.com/homemade-mac-and-cheese/#:~:text=PRINT-,Easy%20Homemade%20Mac%20and%20Cheese%20Recipe,-LAURA", "15m", "8", "271.1", ["vegetarian", "under-fifteen-prep"]);
 
-    addRecipe("Simple Tomato Soup", "'This easy tomato soup is my go-to recipe. It's simple and delicious, ready in 30 minutes, and I give leftovers to my kids for lunch. I usually serve it with rosemary bread for dunking.' - Barbara Sauermann", "../../images/recipes/3tomato-soup.png", "https://www.allrecipes.com/recipe/269276/simple-tomato-soup/#:~:text=TOMATO%20SOUP%20RECIPES-,Simple%20Tomato%20Soup,-4.7", "30m", "100", ["low-carb", "vegetarian", "gluten-free"]);
+    addRecipe("Simple Tomato Soup", "'This easy tomato soup is my go-to recipe. It's simple and delicious, ready in 30 minutes, and I give leftovers to my kids for lunch. I usually serve it with rosemary bread for dunking.' - Barbara Sauermann", "../../images/recipes/3tomato-soup.png", "https://www.allrecipes.com/recipe/269276/simple-tomato-soup/#:~:text=TOMATO%20SOUP%20RECIPES-,Simple%20Tomato%20Soup,-4.7", "30m", "6", "100", ["gluten-free", "low-carb", "vegetarian"]);
 
-    addRecipe("Chicken Noodle Soup", "'Take the time to make Chicken Noodle Soup from scratch and you'll never be able to make it another way again! My mom's version is a step above the rest, made with homemade stock and egg noodles that take just minutes to make.' - Lauren Allen", "../../images/recipes/4chicken-noodle-soup.png", "https://tastesbetterfromscratch.com/chicken-noodle-soup/#:~:text=from%207936%20votes-,Chicken%20Noodle%20Soup,-Take%20the%20time", "40m", "206", ["low-carb"]);
+    addRecipe("Chicken Noodle Soup", "'Take the time to make Chicken Noodle Soup from scratch and you'll never be able to make it another way again! My mom's version is a step above the rest, made with homemade stock and egg noodles that take just minutes to make.' - Lauren Allen", "../../images/recipes/4chicken-noodle-soup.png", "https://tastesbetterfromscratch.com/chicken-noodle-soup/#:~:text=from%207936%20votes-,Chicken%20Noodle%20Soup,-Take%20the%20time", "40m", "8", "206", ["low-carb"]);
+
+    addRecipe("Easy Gluten-Free Broccoli Cheese Soup", "'Craving comfort food? Our Gluten-Free Broccoli Cheese Soup recipe is calling your name! Thick, creamy, cheesy, and delicious.' - Emily Dixon, One Lovely Life", "../../images/recipes/5-cheesy-broccoli-soup.png", "https://www.onelovelylife.com/gluten-free-broccoli-cheese-soup/#tasty-recipes-45299-jump-target:~:text=what%20you%20think!-,easy%20gluten%2Dfree%20broccoli%20cheese%20soup,-5%20Stars", "35m", "4-5", "580", ["gluten-free", "vegetarian"]);
+
+    // 6-10
+    addRecipe("French Toast", "'This classic French toast recipe is an easy, delicious breakfast. I love it with fresh fruit and maple syrup on top!' - Jeanine and Jack", "../../images/recipes/6french-toast.png", "https://www.loveandlemons.com/french-toast/#wprm-recipe-container-43887:~:text=GO-,French%20Toast,-rate%20this%20recipe", "20m", "4", "786", ["breakfast", "vegetarian"]);
+
+    addRecipe("Fried Egg Sandwich", "'This fried egg sandwich makes great comfort food! You can use any type of bread or cheese that you want for this quick and easy breakfast. Serve with fruit and juice and/or milk for a full breakfast.' - Erica", "../../images/recipes/7fried-egg-sandwich.png", "https://www.allrecipes.com/recipe/21004/fried-egg-sandwich/#:~:text=BREAKFAST%20SANDWICH%20RECIPES-,Fried%20Egg%20Sandwich,-4.5", "15m", "4", "386", ["breakfast", "vegetarian", "under-fifteen-prep"]);
+
+    addRecipe("Crispy Hash Browns", "'The secret to the crispiest hash browns? Remove as much moisture as possible before frying. Here's how.' - Elise Bauer", "../../images/recipes/8hashbrowns.png", "https://www.simplyrecipes.com/recipes/crispy_hash_browns/#:~:text=PRINT-,Crispy%20Hash%20Browns,-PREP%20TIME", "20m", "4", "200", ["breakfast", "dairy-free", "vegetarian"]);
+
+    addRecipe("Mamaw's Chicken and Rice Casserole", "'I grew up with this chicken and rice casserole recipe. My Grandmother got it from a lady from church at a potluck a long time ago! She passed it down to my mom and it's always been a family favorite. Quick and easy for school nights, and the leftovers are just as good!' - katiefbenham", "../../images/recipes/9chicken-and-rice-casserole.png", "https://www.allrecipes.com/recipe/233983/mamaws-chicken-and-rice-casserole/#:~:text=Keep%20screen%20awake)-,Ingredients,-1X", "1h 20m", "6", "411", ["gluten-free"]);
+
+    addRecipe("Sloppy Joes", "'These crowd-pleasing Sloppy Joe sandwiches will take you back to your childhood! This is my mom's recipe and it always gets compliments!' - Tamara", "../../images/recipes/10sloppy-joes.png", "https://www.allrecipes.com/recipe/24264/sloppy-joes-ii/#:~:text=Keep%20screen%20awake)-,Ingredients,-1X", "35m", "6", "243", ["dairy-free", "low-carb"]);
+    
+    //11-15
+    addRecipe("Easy Chicken Fajitas", "'These Easy Chicken Fajitas are the perfect weeknight meal because they’re on the table in about 20 minutes start to finish!' - Holly Nilsson", "../../images/recipes/11chicken-fajitas.png", "https://www.spendwithpennies.com/easy-chicken-fajitas/#:~:text=comment%2C%20click%20here!-,Easy%20Chicken%20Fajitas,-These%20Easy%20Chicken", "30m", "4", "1032", ["dairy-free", "gluten-free"]);
+
+    addRecipe("Easy Ground Beef Tacos", "'These Ground Beef Tacos are filled with juicy flavorful taco meat made with my homemade taco seasoning - it's an easy weeknight recipe for the whole family!' - Yumna Jawad", "../../images/recipes/12ground-beef-tacos.png", "https://feelgoodfoodie.net/recipe/ground-beef-tacos-napa-cabbage-guacamole/#wprm-recipe-container-5870:~:text=on%20my%20stories!-,Ground%20Beef%20Tacos,-These%20Ground%20Beef", "25m", "4", "142", ["dairy-free", "low-carb"]);
+    
+    addRecipe("Easy Black Bean Soup", "'A simple and healthy soup made with canned black beans and ingredients you probably already have in your pantry! This flavorful black bean soup is vegan, gluten-free, and vegetarian.' - Maria Lichty", "../../images/recipes/13black-bean-soup.png", "https://www.twopeasandtheirpod.com/easy-black-bean-soup/#:~:text=SOUP-,Easy%20Black%20Bean%20Soup,-Easy%20Black%20Bean", "45m", "6", "428", ["gluten-free", "vegetarian"]);
+
+    addRecipe("Easy Chicken Stir Fry Recipe", "'This easy Chicken Stir Fry recipe is loaded with fresh veggies and the most delicious sauce made with honey, soy sauce, and toasted sesame oil! This healthy recipe takes 20 minutes to make and will wow your family with it’s amazing flavor!' - Trish", "../../images/recipes/14chicken-stir-fry.png", "https://www.momontimeout.com/easy-chicken-stir-fry-recipe/#:~:text=from%20821%20votes-,Chicken%20Stir%20Fry%20Recipe,-This%20easy%C2%A0Chicken", "18m", "4", "343", ["gluten-free"]);
+
+    addRecipe("Stuffed Bell Peppers", "'Stuffed Bell Peppers have been a huge favorite of mine since my aunt first made them for me many many years ago. At the time it seemed like such an elaborate, fancy recipe, but who knew they were really this simple to make! It’s a classic comfort food recipe that’s hearty, filling, easy to customize, and can feed a large family.' - Marsha McDougal", "../../images/recipes/15stuffed-bell-peppers.png", "https://www.budgetbytes.com/stuffed-bell-peppers/#:~:text=SHARE%20THIS%20RECIPE-,STUFFED%20BELL%20PEPPERS,-4.59%20from%2092", "1h 5m", "6", "454", ["gluten-free"]);
+    //16-20
+
+
+    //21-25
 }
 const loadSelectorOptions = () =>{
     selectFilter.replaceChildren() //Clears Options
@@ -188,8 +217,9 @@ const displayRecipe = (inputRecipe) =>{
     const imagePath =   inputRecipe[2];
     const link =        inputRecipe[3];
     const prepTime =    inputRecipe[4];
-    const calories =    inputRecipe[5];
-    const tags =        inputRecipe[6];
+    const servings =    inputRecipe[5]
+    const calories =    inputRecipe[6];
+    const tags =        inputRecipe[7];
     //--Recipe Block
     const recipe = document.createElement("div");
     recipe.setAttribute("class", "recipe");
@@ -219,6 +249,8 @@ const displayRecipe = (inputRecipe) =>{
     
     //Prep Time
     tagList.appendChild(createTag(("Prep Time: " + prepTime), true));
+    //Servings
+    tagList.appendChild(createTag((servings + " servings"), true));
     //Calories
     tagList.appendChild(createTag((calories + " cal"), true));
 
