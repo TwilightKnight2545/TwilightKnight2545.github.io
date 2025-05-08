6e2940e10d5d70fde52f4660a332a504c38cd4db
Matthew Wickstrom <17770557@northwoodtech.edu>
2025-05-08 12:32:58 -0500

    deleted:    .vscode/launch.json 	modified:   MJ_AW_FA_17770557/js/budget.js 	modified:   MJ_AW_FA_17770557/pages/budgeting/index.html 	modified:   MJ_AW_FA_17770557/styles/budget.css

--- MJ_AW_FA_17770557/js/budget.js
+++ MJ_AW_FA_17770557/js/budget.js
@@ -3,8 +3,55 @@
 const getElement = (selector) => document.querySelector(selector);
 
 //Helper Functions
-
+const formatCurrency = (number) => {return new Intl.NumberFormat("en-US", {style:'currency', currency: "USD"}).format(number)}
+const formatNumber = (currency) => {return parseFloat(currency.replace(/[^\d\.]/g, ""));}
 //Functions
+//On mouse Down
+const formatElementNumber = (element) => {
+    element.value = formatNumber(element.value)
+    if(parseFloat(element.value) == 0.0)
+        element.value = "";
+} //Called on mousedown, removes dollar sign
+const formatInputNumber = (element) => {
+    if(element.value != ""){
+        element.value = formatNumber(element.value)
+        if(parseFloat(element.value) == 0.0)
+            element.value = "";
+    }
+} //Called on mousedown, removes dollar sign
+
+//Focus Out
+const formatSavingsOutputs = (element) =>{
+    element.value = (isNaN(element.value) || element.value == "" || parseFloat(element.value) < 0) ? formatCurrency("0.0") : formatCurrency(element.value);
+    if(formatNumber(element.value) > formatNumber(outputNetIncome.value)){ //Can only set aside at most the net income
+        element.value = outputNetIncome.value;
+    }
+    //Set the other field to the remainder of Net Income - This Field
+    if(element == outputSavings){
+        outputDisposableIncome.value = formatCurrency(formatNumber(outputNetIncome.value) - formatNumber(element.value))
+    }
+    else{
+        outputSavings.value = formatCurrency(formatNumber(outputNetIncome.value) - formatNumber(element.value))
+    }
+    //Format % Saved
+    outputSavingPercent.innerText = ((formatNumber(outputSavings.value) / formatNumber(outputNetIncome.value)) * 100) + "%";
+} //Called on focusout, formats to currency
+const formatInputs = (element) =>{
+    if(element.value != ''){
+        element.setAttribute("class", ""); //Initially valid
+        if(isNaN(element.value.replace("$", "")))
+            element.setAttribute("class", "invalid-entry");
+
+        //Check if it should be a currency or not
+        if(element.getAttribute("int") == "true"){
+            element.value = (isNaN(element.value) || parseFloat(element.value) < 0) ? "" : Math.ceil(parseFloat(element.value));
+        }
+        else{
+            element.value = (isNaN(element.value) || parseFloat(element.value) < 0) ? "" : formatCurrency(element.value);
+        }
+    }
+        
+} //Called on focusout, formats to currency
 //--Switch Income Type
 
 const useWage = () =>{
@@ -26,9 +73,34 @@ const useSalary = () =>{
     divWageTab.setAttribute("style", "display: none;")
 }
 const calcDisposableIncome = () =>{
-    const netIncome = parseDouble(outputNetIncome.replace("$", ""));
-    alert(netIncome)
+    const netIncome = parseFloat(outputNetIncome.value.replace("$", ""));
+    const percentSaved = parseFloat(outputSavingPercent.innerText.replace("%", "")) / 100;
     
+    outputSavings.value = formatCurrency((netIncome * percentSaved).toFixed(2));
+    outputDisposableIncome.value = formatCurrency((netIncome * (1 - percentSaved)).toFixed(2));
+}
+const calcTotalIncome = () =>{
+    if(buttonWages.getAttribute("class") == "selected"){
+        if(inputHourlyWage.value != "" && inputHoursPerWeek.value != ""){
+            outputMonthlyIncome.value = formatCurrency(formatNumber(inputHourlyWage.value) * parseFloat(inputHoursPerWeek.value) * 4.3452381/*Weeks in a month*/ );
+            outputAnnualIncome.value = formatCurrency(formatNumber(outputMonthlyIncome.value) * 12);
+        }
+        else{
+            outputMonthlyIncome.value = "";
+            outputAnnualIncome.value = "";
+        }
+    }
+    else{
+        outputMonthlyIncome.value = formatCurrency(formatNumber(inputAnnualSalary.value) / 12);
+    }
+}
+const calcTotalExpenses = () =>{
+
+}
+const calcAnnual = () =>{
+    outputNetIncomeAnnual.value = formatCurrency(formatNumber(outputNetIncome.value) * 12);
+    outputSavingsAnnual.value = formatCurrency(formatNumber(outputSavings.value) * 12);
+    outputDisposableIncomeAnnual.value = formatCurrency(formatNumber(outputDisposableIncome.value) * 12);
 }
 //Page Elements
 //--Divs
@@ -59,15 +131,64 @@ const outputMonthlyIncome = getElement("#monthly-income");
 const outputAnnualIncome = getElement("#annual-income");
 
 const outputNetIncome = getElement("#net-income");
+const outputNetIncomeAnnual = getElement("#annual-net-income");
+const outputSavingPercent = getElement("#percent-saved");
+
 const outputSavings = getElement("#savings");
-const outputSavingPercent = getElement("percent-saved");
+const outputSavingsAnnual = getElement("#annual-savings")
+
 const outputDisposableIncome = getElement("#disposable-income");
+const outputDisposableIncomeAnnual = getElement("#annual-disposable-income");
 //--Labels
 const labelAnnualIncome = getElement("#annual-income-label");
 //Event Listener
-document.addEventListener("DOMContentLoaded", ()=>{
-    buttonWages.addEventListener("click", useWage);
-    buttonSalary.addEventListener("click", useSalary);
+document.addEventListener("DOMContentLoaded", () => {
+    //Inputs
+    //--Income
+    buttonWages.addEventListener("click", function(){useWage(); calcTotalIncome();});
+    buttonSalary.addEventListener("click", function(){useSalary(); calcTotalIncome();});
 
-    rangePercentSaved.addEventListener("input", calcDisposableIncome); //currently broken, ask Rene
+    inputHourlyWage.addEventListener("mousedown", function(){formatInputNumber(inputHourlyWage);})
+    inputHourlyWage.addEventListener("focusout", function(){formatInputs(inputHourlyWage); calcTotalIncome()})
+
+    inputHoursPerWeek.addEventListener("mousedown", function(){formatInputNumber(inputHoursPerWeek);})
+    inputHoursPerWeek.addEventListener("focusout", function(){formatInputs(inputHoursPerWeek); calcTotalIncome()})
+    
+    inputAnnualSalary.addEventListener("mousedown", function(){formatInputNumber(inputAnnualSalary);})
+    inputAnnualSalary.addEventListener("focusout", function(){formatInputs(inputAnnualSalary); calcTotalIncome()})
+    //--Expenses
+    //----Housing
+    inputRent.addEventListener("mousedown", function(){formatInputNumber(inputRent);})
+    inputRent.addEventListener("focusout", function(){formatInputs(inputRent); calcTotalExpenses()})
+
+    inputElectric.addEventListener("mousedown", function(){formatInputNumber(inputElectric);})
+    inputElectric.addEventListener("focusout", function(){formatInputs(inputElectric); calcTotalExpenses()})
+
+    inputHeating.addEventListener("mousedown", function(){formatInputNumber(inputHeating);})
+    inputHeating.addEventListener("focusout", function(){formatInputs(inputHeating); calcTotalExpenses()})
+
+    inputWater.addEventListener("mousedown", function(){formatInputNumber(inputWater);})
+    inputWater.addEventListener("focusout", function(){formatInputs(inputWater); calcTotalExpenses()})
+    //----Utilities
+    inputInternet.addEventListener("mousedown", function(){formatInputNumber(inputInternet);})
+    inputInternet.addEventListener("focusout", function(){formatInputs(inputInternet); calcTotalExpenses()})
+    
+    inputPhone.addEventListener("mousedown", function(){formatInputNumber(inputPhone);})
+    inputPhone.addEventListener("focusout", function(){formatInputs(inputPhone); calcTotalExpenses()})
+    //----Misc. Expenses
+    inputFood.addEventListener("mousedown", function(){formatInputNumber(inputFood);})
+    inputFood.addEventListener("focusout", function(){formatInputs(inputFood); calcTotalExpenses()})
+
+    inputGas.addEventListener("mousedown", function(){formatInputNumber(inputGas);})
+    inputGas.addEventListener("focusout", function(){formatInputs(inputGas); calcTotalExpenses()})
+
+    //Results
+    rangePercentSaved.addEventListener("input", function(){
+        calcDisposableIncome();
+        calcAnnual();
+    });
+    outputSavings.addEventListener("mousedown", function(){formatElementNumber(outputSavings);})
+    outputSavings.addEventListener("focusout", function(){formatSavingsOutputs(outputSavings); calcAnnual();})
+    outputDisposableIncome.addEventListener("mousedown", function(){formatElementNumber(outputDisposableIncome);})
+    outputDisposableIncome.addEventListener("focusout", function(){formatSavingsOutputs(outputDisposableIncome); calcAnnual();})
 })
